6f1f0f31a9ccc9182f1ce1785edbdd2ef134670e
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-17 08:15:08 -0300

    api post users + password

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,4 +1,6 @@
 import dbConnect from '../../src/utils/dbConnect';
+import { crypto } from '../../src/utils/password';
+import UsersModel from '../../src/models/users';
 
 const users = async (req, res) => {
   const { method } = req
@@ -8,6 +10,32 @@ const users = async (req, res) => {
       await dbConnect()
       res.status(200).json({ success: true })
       break
+    case 'POST':
+      // pegar os dados que vem no req
+      const {
+        name,
+        email,
+        password
+      } = req.body
+
+      // conectar no banco 
+      await dbConnect()
+
+      // criptografar senha
+      const passwordCrypto = await crypto(password)
+
+      const user = new UsersModel({
+        name,
+        email,
+        password: passwordCrypto,
+      })
+
+      // salvar os dados
+      user.save()
+
+      // responder sucesso
+      res.status(201).json({ success: true })
+
   }
 }
 
