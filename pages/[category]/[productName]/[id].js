bc7d78e7fa5641b2ad3ccbb94a3bcd14f4b27882
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-21 10:30:22 -0300

    Pagina de Produto Responsiva

--- pages/[category]/[productName]/[id].js
+++ pages/[category]/[productName]/[id].js
@@ -8,6 +8,7 @@ import {
   Container,
   Grid,
   Typography,
+  useMediaQuery,
 } from '@mui/material';
 import Carousel from 'react-material-ui-carousel'
 
@@ -18,14 +19,18 @@ import dbConnect from '../../../src/utils/dbConnect';
 import { formatCurrency } from '../../../src/utils/currency';
 
 const Product = ({ product }) => {
+
+  const smDown = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  const mdUp = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
   return (
     <TemplateDefault>
       <Container maxWidth='lg'>
         <Grid container spacing={3}>
-          <Grid item xs={8}>
+          <Grid item xs={12} sm={12} md={8} lg={8}>
             <Box
               bgcolor={theme.palette.background.default}
-              sx={{ padding: 3, marginBottom: 3 }}
+              sx={smDown ? { padding: 1, marginBottom: 3 } : { padding: 3, marginBottom: 3 }}
             >
               <Carousel autoPlay={false} animation='slide' navButtonsAlwaysVisible indicators={false}>
                 {
@@ -72,7 +77,7 @@ const Product = ({ product }) => {
             </Box>
           </Grid>
 
-          <Grid item xs={4}>
+          <Grid item xs={12} sm={12} md={4} lg={4}>
             <Card
               elevation={0}
               sx={{ backgroundColor: theme.palette.background.default, padding: 1, marginBottom: 3 }}
