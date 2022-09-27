13e1e3f67e237678f8702cf685e4f4baf82db468
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-27 15:56:52 -0300

    Teste for index in prod

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export async function getServerSideProps() {
 
   const products = await ProductsModel.aggregate([{
     $sample: { size: 9 }
-  }]).sort({ publishDate: -1 })
+  }])
 
   // const products = await ProductsModel.find().sort({ publishDate: -1 })
 
