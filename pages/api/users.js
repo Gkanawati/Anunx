2f15af07187f709aa324f5752fb5ee6ccbf71140
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-27 13:45:21 -0300

    Next configs to deploy

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,10 +1,16 @@
-import nextConnect from 'next-connect';
-import { get, post } from '../../src/controllers/users';
+import nextConnect from 'next-connect'
+import { get, post } from '../../src/controllers/users'
 
-const route = nextConnect()
-
-route.get(get)
-
-route.post(post)
+const route = nextConnect({
+  onError: (err, req, res, next) => {
+    console.error(err);
+    res.status(500).end("Something broke!");
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end("Page is not found");
+  },
+})
+  .get(get)
+  .post(post)
 
 export default route
