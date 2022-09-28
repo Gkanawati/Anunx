ff9bfc3ac672a4ba1eb90147eb9e655dabaf3c43
Gkanawati <gabrielkanawati3@gmail.com>
2022-09-28 11:02:03 -0300

    Alterando num de Prod na Home

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,10 @@ const Home = ({ products }) => {
 export async function getServerSideProps() {
   await dbConnect()
 
-  const products = await ProductsModel.aggregate([{ $sample: { size: 8 } }, { $sort: { publishDate: -1 } }])
+  const products = await ProductsModel.aggregate([
+    { $sample: { size: 7 } },
+    { $sort: { publishDate: -1 } }
+  ])
 
   // const products = await ProductsModel.find().limit(6).sort({ publishDate: -1 })
 
