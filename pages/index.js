abbad8dee98d88831424bd6509dc1fba7425e334
Gabriel Loutfalla Kanawati <gabriel.rosario@melicontrol.com.br>
2023-10-17 08:54:19 -0300

    change home products order list

--- pages/index.js
+++ pages/index.js
@@ -55,9 +55,9 @@ const Home = ({ products }) => {
 export async function getServerSideProps() {
   await dbConnect()
 
-  // const products = await ProductsModel.find().limit(6).sort({ publishDate: -1 })
+  const products = await ProductsModel.find().limit(6).sort({ publishDate: -1 })
 
-  const products = await ProductsModel.aggregate([{ $sample: { size: 6 } }])
+  // const products = await ProductsModel.aggregate([{ $sample: { size: 6 } }])
 
   return {
     props: {
